d694648fae829ea412fffce37047c76ef4f4e1ae
Fernando Muniz <fernando@kasta.io>
2025-10-28 20:17:56 +0000

    chore: update database configuration to support SSL with custom CA

--- infra/database.js
+++ infra/database.js
@@ -7,7 +7,7 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     password: process.env.POSTGRES_PASSWORD,
     database: process.env.POSTGRES_DB,
-    ssl: process.env.NODE_ENV === "production" ? true : false,
+    ssl: getSSLValues(),
   });
   try {
     await client.connect();
@@ -19,6 +19,15 @@ async function query(queryObject) {
   } finally {
     await client.end();
   }
+}
+
+function getSSLValues() {
+  if (process.env.POSTGRES_CA) {
+    return {
+      ca: process.env.POSTGRES_CA,
+    };
+  }
+  return process.env.NODE_ENV === "production" ? true : false;
 }
 
 export default { query: query };
